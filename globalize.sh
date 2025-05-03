651a64e63465dba2124d432008b19e9cdf4992da
Wesley Yan S. B. <102262759+simplyYan@users.noreply.github.com>
2025-05-03 16:47:49 +0000

    Update globalize.sh

--- globalize.sh
+++ globalize.sh
@@ -17,6 +17,31 @@ check_command() {
 # Directory where the tools were cloned
 TOOLS_DIR=$(pwd)
 
+install_tool() {
+    local tool_name=$1
+    local tool_dir=$2
+    local install_cmd=$3
+    
+    echo "Installing $tool_name..."
+    if [ ! -d "$tool_dir" ]; then
+        echo "Error: Directory $tool_dir not found in $TOOLS_DIR"
+        exit 1
+    fi
+    
+    cd "$tool_dir" || { echo "Error: Could not access $tool_dir"; exit 1; }
+    eval "$install_cmd"
+    check_command "Installation of $tool_name"
+}
+
+# install tools
+install_tool "set" "social-engineer-toolkit" "pip3 install -r requirements.txt"
+install_tool "wifite2" "wifite2" "sudo python3 setup.py install"
+install_tool "mitmproxy" "mitmproxy" "pip3 install ."
+install_tool "xsstrike" "XSStrike" "pip3 install -r requirements.txt"
+install_tool "cewl" "CeWL" "sudo gem install bundler && bundle install"
+install_tool "dirhunt" "dirhunt" "pip3 install -r requirements.txt"
+install_tool "subfinder" "subfinder" "go get -v ./... && go build -o subfinder . && sudo cp subfinder /usr/local/bin/"
+
 # List of tools and their directories
 declare -A tools
 tools=(
